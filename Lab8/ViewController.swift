a29728d4bac2a95ae33a725d818704eb4bdfb0ff
Daniil Paskal <pask.daniil@gmail.com>
2024-03-15 21:27:57 -0400

    Initial Commit

---
+++ Lab8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab8
+//
+//  Created by user237236 on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
